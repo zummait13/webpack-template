51991fca43087849406feb9bf9606115c80dfc61
zummait13 <34415872+zummait13@users.noreply.github.com>
2024-10-14 11:17:14 +0300

    Create template

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import {} from "";
+import "./style_reset.css";
+import "./styles.css";
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
