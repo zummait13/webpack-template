0fa1c7e6634c9c607e1bd47ac22b8d1b6417286f
zummait13 <34415872+zummait13@users.noreply.github.com>
2024-11-01 15:59:26 +0300

    Fix error with extra react

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
     '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
 
-    '^react(.*)$': '<rootDir>/vendor/react-master$1',
     '^config$': '<rootDir>/configs/app-config.js',
   },
 };
